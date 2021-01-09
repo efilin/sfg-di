b5783ef8ad4b503e852f49b0ea7013621b609562
efilin <2727fas@gmail.com>
2021-01-09 12:27:31 +0300

    Property Source

--- src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
+++ src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
@@ -1,6 +1,7 @@
 package guru.springframework.sfgdi;
 
 import guru.springframework.sfgdi.controllers.*;
+import guru.springframework.sfgdi.examplebeans.FakeDataSource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -11,16 +12,20 @@ public class SfgDiApplication {
     public static void main(String[] args) {
         ApplicationContext ctx = SpringApplication.run(SfgDiApplication.class, args);
 
-        PetController petController = ctx.getBean("petController", PetController.class);
+       /* PetController petController = ctx.getBean("petController", PetController.class);
         System.out.println("--- The Best Pet is ---");
         System.out.println(petController.whichPetIsTheBest());
 
         I18nController i18nController = (I18nController) ctx.getBean("i18nController");
-        System.out.println(i18nController.sayHello());
+        System.out.println(i18nController.sayHello());*/
 
         MyController myController = (MyController) ctx.getBean("myController");
 
-        System.out.println("-------------Primary Bean");
+        FakeDataSource dataSource = (FakeDataSource) ctx.getBean(FakeDataSource.class);
+
+        System.out.println(dataSource.getUser());
+
+       /* System.out.println("-------------Primary Bean");
         System.out.println(myController.sayHello());
 
         System.out.println("-------------Property");
@@ -39,7 +44,7 @@ public class SfgDiApplication {
 
         ConstructorInjectedController constructorInjectedController = (ConstructorInjectedController) ctx.getBean("constructorInjectedController");
 
-        System.out.println(constructorInjectedController.getGreeting());
+        System.out.println(constructorInjectedController.getGreeting());*/
 
     }
 
